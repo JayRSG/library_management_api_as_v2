d4040df4683e4ef304758125a6b02051f35c00b4
Jay R SG <jaysen110@gmail.com>
2023-07-19 14:07:35 +0600

    implementing search option in show_books

--- app/books/show_books.php
+++ app/books/show_books.php
@@ -16,31 +16,46 @@ if (!auth()) {
 try {
   $all = $_GET['all'] ?? null;
   $id = $_GET['id'] ?? null;
+  $search_term = $_GET['search_term'] ?? null;
   $sql = "";
+  $params = [];
+  $stmt = "";
+  $result = "";
 
   if ($all) {
     $sql = "SELECT * FROM book";
-    $stmt = $conn->prepare($sql);
-  } else if ($id) {
-    $sql = "SELECT * FROM book where id = :id";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(":id", $id);
+  } else if ($id || $search_term) {
+    $sql = "SELECT * FROM book where ";
+    if ($id) {
+      $sql .= "id = :id LIMIT 1";
+    } else if ($search_term) {
+      $sql .= "name LIKE :search_term OR author LIKE :search_term OR publisher LIKE :search_term OR isbn = :search_term";
+    }
   }
 
-  $stmt->execute();
+  $sql = rtrim($sql, "OR ");
 
-  if ($id) {
-    $book = $stmt->fetch(PDO::FETCH_ASSOC);
-  } else if ($all) {
-    $book = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  if ($sql != "") {
+    $stmt = $conn->prepare($sql);
+    if ($id) {
+      $stmt->bindParam(':id', $id);
+    }
+    if ($search_term) {
+      $stmt->bindValue(':search_term', '%' . $search_term . '%');
+    }
+    $result = $stmt->execute();
+    if ($id) {
+      $book = $stmt->fetch(PDO::FETCH_ASSOC);
+    } else if ($search_term) {
+      $book = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
   }
 
-
-  if ($stmt->rowCount() > 0) {
+  if ($result && $stmt->rowCount() > 0) {
     response(['data' => $book], 200);
   } else {
-    response(['message' => "Book Not Found"], 404);
+    response(['message' => "Book Not Found", $sql], 404);
   }
-} catch (\PDOException $e) {
-  response(['message' => $e->getMessage()], 500);
+} catch (PDOException $e) {
+  response(['message' => $e->getMessage(), $sql], 500);
 }
