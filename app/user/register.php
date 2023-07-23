079c0c5fabbebb4ee4149d37a2d805bb4ae47c41
Jay R SG <jaysen110@gmail.com>
2023-07-24 04:24:20 +0600

    modifying register to use phone

--- app/user/register.php
+++ app/user/register.php
@@ -8,7 +8,7 @@ function register_validator($data)
     !isset($data['email']) ||
     !isset($data['password']) ||
     !isset($data['student_id']) ||
-    // !isset($data['semester']) ||
+    !isset($data['phone']) ||
     !isset($data['department'])
   ) {
     return false;
@@ -42,11 +42,11 @@ try {
   $password = $_POST['password'] ? password_hash($_POST['password'], PASSWORD_DEFAULT) : null;
   $student_id = $_POST['student_id'] ?? null;
   $fingerprint = $_POST['fingerprint'] ?? null;
-  // $semester = $_POST['semester'] ?? null;
+  $semester = $_POST['phone'] ?? null;
   $department = $_POST['department'] ?? null;
 
-  $sql = "INSERT INTO `user` (`first_name`, `last_name`, `email`, `password`, `student_id`, `department`) 
-          VALUES (:first_name, :last_name, :email, :pass, :student_id, :department)";
+  $sql = "INSERT INTO `user` (`first_name`, `last_name`, `email`, `phone`, `password`, `student_id`, `department`) 
+          VALUES (:first_name, :last_name, :email, :phone, :pass, :student_id, :department)";
   $stmt = $conn->prepare($sql);
 
   $stmt->bindParam(':first_name', $firstName, PDO::PARAM_STR);
@@ -54,7 +54,7 @@ try {
   $stmt->bindParam(':email', $email, PDO::PARAM_STR);
   $stmt->bindParam(':pass', $password, PDO::PARAM_STR);
   $stmt->bindParam(':student_id', $student_id, PDO::PARAM_STR);
-  // $stmt->bindParam(':semester', $semester, PDO::PARAM_INT);
+  $stmt->bindParam(':phone', $phone, PDO::PARAM_INT);
   $stmt->bindParam(':department', $department, PDO::PARAM_STR);
   // $stmt->bindParam(':fingerprint', $fingerprint, PDO::PARAM_STR);
 
